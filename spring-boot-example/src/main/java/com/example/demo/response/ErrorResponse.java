866c053d702adc1e42b1a681a4131943cd4f7815
PattuRajesh <97498414+PattuRajesh@users.noreply.github.com>
2022-02-17 20:29:33 -0800

    Add files via upload

---
+++ spring-boot-example/src/main/java/com/example/demo/response/ErrorResponse.java
@@ -0,0 +1,38 @@
+package com.example.demo.response;
+
+import java.util.Date;
+import java.util.List;
+
+public class ErrorResponse {
+	private Date timestamp;
+    private String message;
+    private List<String> details;
+    
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public List<String> getDetails() {
+		return details;
+	}
+	public void setDetails(List<String> details) {
+		this.details = details;
+	}
+	public ErrorResponse(Date timestamp, String message, List<String> details) {
+		super();
+		this.timestamp = timestamp;
+		this.message = message;
+		this.details = details;
+	}
+	
+   
+
+}
